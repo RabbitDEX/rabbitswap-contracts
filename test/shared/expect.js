37cfeb8f18567cb84a813cff63a900221e1e4a5e
Vicky <vickydev.viction@gmail.com>
2025-04-28 10:40:43 +0700

    chore: remove compiled js files

--- test/shared/expect.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.expect = void 0;
-const chai_1 = require("chai");
-Object.defineProperty(exports, "expect", { enumerable: true, get: function () { return chai_1.expect; } });
